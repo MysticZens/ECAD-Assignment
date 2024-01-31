fd979c20553c7141d044e40be6e61fdf53e062c6
Ng Qian Zhe <s10243009@connect.np.edu.sg>
2024-01-31 15:13:47 +0800

    Simple filter rating system

--- feedback.php
+++ feedback.php
@@ -4,10 +4,22 @@ include("header.php"); // Include the Page Layout header
 include_once("mysql_conn.php");
 ?>
 
-<div class="table-responsive" style="width: 80%; margin: auto">
+<div class="table-responsive" style="width: 70%; margin: auto">
 	<div style="margin-bottom:20px;">
 		<a href="createFeedback.php"><button class="submitbutton">Review</button></a>
-	</div>
+		<form method="post" style="display: flex; align-items: center; float: right;">
+			<label for="filterRating">Filter by Rating:</label>
+				<select id="filterRating" name="filterRating" style="margin-right: 30px">
+					<option value="all">All Ratings</option>
+					<option value="1">1 star</option>
+					<option value="2">2 stars</option>
+					<option value="3">3 stars</option>
+					<option value="4">4 stars</option>
+					<option value="5">5 stars</option>
+				</select>  
+				<button class="submitbutton" type="submit">Filter</button>     
+		</form>
+  	</div>
     <table class="table table-striped table-bordered" style="white-space:nowrap;">
     <thead class="table" style="text-align: center;">
 		<tr>
@@ -21,10 +33,26 @@ include_once("mysql_conn.php");
 	<tbody class="table">
 		<!-- Table Body -->
 		<?php
-			$qry = "SELECT s.Name, f.* FROM feedback f 
-					INNER JOIN shopper s 
-					ON s.ShopperID = f.ShopperID";
-			$result = $conn->query($qry);
+			if (isset($_POST["filterRating"]) && $_POST["filterRating"] != "all") {
+				$qry = "SELECT s.Name, f.* FROM feedback f 
+						INNER JOIN shopper s 
+						ON s.ShopperID = f.ShopperID
+						WHERE Rank=?
+						ORDER BY Rank DESC
+						LIMIT 10";
+				$stmt = $conn->prepare($qry);
+				$stmt->bind_param("i", $_POST["filterRating"]);
+				$stmt->execute();
+				$result = $stmt->get_result();
+			}
+			else {
+				$qry = "SELECT s.Name, f.* FROM feedback f 
+						INNER JOIN shopper s 
+						ON s.ShopperID = f.ShopperID
+						ORDER BY Rank DESC
+						LIMIT 10";
+				$result = $conn->query($qry);
+			}
   			if ($result->num_rows > 0) {
 				// Output data of each row
 				while($row = $result->fetch_array()) {
@@ -43,7 +71,7 @@ include_once("mysql_conn.php");
 				}
 			} 
 			else {
-				echo "<tr><td colspan='4'><p style='color: red; font-size: 25px; text-align: center; background-color: #ffbebe'><b>No records found!</b><p></td></tr>";
+				echo "<tr><td style='background-color: #ffbebe' colspan='5'><p style='color: red; font-size: 25px; text-align: center; background-color: #ffbebe'><b>No records found!</b></p></td></tr>";
 			}
   		?>
     </tbody>
