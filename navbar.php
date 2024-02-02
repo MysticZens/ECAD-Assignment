2ad98f3b451eda11c5c0a7fdbcbed80bf271bc8e
Xavier Lim <s10242016@connect.np.edu.sg>
2024-02-02 12:43:55 +0800

    edited comments for navbar.php

--- navbar.php
+++ navbar.php
@@ -8,7 +8,6 @@ $content2 = "<li class='nav-item'>
 		     <a class='nav-link' href='login.php'>Login</a></li>";
 
 if(isset($_SESSION["ShopperName"])) { 
-	//To Do 1 (Practical 2) -
     //Display a greeting message, Change Password and logout links 
     //after shopper has logged in.
     $content1 = "Welcome to Giftown Singapore! (<b>$_SESSION[ShopperName]</b>)";
@@ -19,16 +18,13 @@ if(isset($_SESSION["ShopperName"])) {
                  <li class='nav-item'>
 		         <a class='nav-link' href='shoppingCart.php'><i class='fa-solid fa-cart-shopping' style='font-size: 20px;'></i></a></li>";
 
-	
-	//To Do 2 (Practical 4) - 
     //Display number of item in cart
     if (isset($_SESSION["NumCartItem"])) {
         $content1 .= ", $_SESSION[NumCartItem] item(s) in shopping cart";
     }
 }
 ?>
-<!-- To Do 3 (Practical 1) - 
-     Display a navbar which is visible before or after collapsing -->
+<!-- Display a navbar which is visible before or after collapsing -->
 <nav class="navbar navbar-expand-md navbar-dark bg-custom">
     <div class="container-fluid">
         <!-- Dynamic Text Display -->
@@ -43,8 +39,7 @@ if(isset($_SESSION["ShopperName"])) {
         </button>
     </div>
 </nav>
-<!-- To Do 4 (Practical 1) - 
-     Define a collapsible navbar -->
+<!-- Define a collapsible navbar -->
 <nav class="navbar navbar-expand-md navbar-dark bg-custom"> 
     <div class="container-fluid">
         <!-- Collapsible part of navbar -->
