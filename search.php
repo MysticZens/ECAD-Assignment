52359b045d341f7455b5d399c6fd6091310b95ad
Muhammad Isaac Mohamed Khaizad <s10242151@connect.np.edu.sg>
2024-02-03 21:53:32 +0800

    Changed functionalities for search (can do 1 by 1 or 2 by 2 or wtv faak)

--- search.php
+++ search.php
@@ -113,6 +113,180 @@ if ((isset($_GET["keywords"]) && trim($_GET['keywords']) != "") &
     }
 	// To Do (DIY): End of Code
 }
+
+
+// PRODUCT TITLE/DESC + PRICE RANGE
+else if ((isset($_GET["keywords"]) && trim($_GET['keywords']) != "") & 
+    (isset($_GET["keywords2"]) && trim($_GET['keywords2']) != "") &
+    (isset($_GET["keywords3"]) && trim($_GET['keywords3']) != "")){
+    // To Do (DIY): Retrieve list of product records with "ProductTitle" 
+    $keywords = '%' . $_GET["keywords"] . '%';
+    $result = $_GET["keywords"];
+    $keywords2 = intval($_GET["keywords2"]);  // Convert to integer
+    $result2 = $_GET["keywords2"];
+    $keywords3 = intval($_GET["keywords3"]);  // Convert to integer
+    $result3 = $_GET["keywords3"];
+    echo "Search results for Title/Description : <b>" . $result . "</b>| Maximum Price : $<b>" . $result2 ."</b>| Minimum Price : $<b>" . $result3 . "</b></br>";
+    
+    $qry = "SELECT p.ProductID, p.ProductTitle, p.ProductDesc, p.ProductImage, p.Offered, p.Price, p.OfferedPrice,
+    CASE WHEN p.Offered = 1 THEN p.OfferedPrice ELSE p.Price END AS CurrentPrice,
+    ps.SpecVal, ps.SpecID 
+    FROM product p 
+    INNER JOIN productspec ps ON p.ProductID = ps.ProductID
+    WHERE (p.ProductTitle LIKE '%$keywords%' OR p.ProductDesc LIKE '%$keywords%') 
+    AND ps.SpecID = 1
+    AND ((p.Offered = 1 AND p.OfferedPrice <= $keywords2) OR (p.Offered = 0 AND p.Price <= $keywords2))
+    AND ((p.Offered = 1 AND p.OfferedPrice >= $keywords3) OR (p.Offered = 0 AND p.Price >= $keywords3))
+    ORDER BY ProductTitle";
+
+ 
+    $result = mysqli_query($conn, $qry);
+
+    if (mysqli_num_rows($result) > 0) {     
+        // Output the results in a table
+        echo "<table>";
+        while ($row = mysqli_fetch_assoc($result)) {
+            $productDetailsLink = "productDetails.php?pid=$row[ProductID]";
+            $img = "./Images/products/$row[ProductImage]";
+
+            echo "<img src='$img' style='width: 300px; max-width: 200px;' />";
+            echo "<div style='padding: 3%; margin-bottom: 20px;'>";
+            echo "<h3><a style='text-decoration:none' href='$productDetailsLink'>$row[ProductTitle]</a></h3>";
+            echo "<p><strong>Description:</strong> $row[ProductDesc]</p>";
+        
+            // Displaying discounted price with strike-through for the original price
+            if ($row['Offered'] == 1) {
+                echo "<p><strong>Original Price </strong> <del>$$row[Price]</del></p>";
+                echo "<p style='color:red;'><b>NOW</b> <strong>$$row[OfferedPrice]</strong></p>";
+            } else {
+                echo "<p><strong>Price</strong> $$row[Price]</p>";
+            }
+        }
+        echo "</table>";
+    } else {
+        echo "<br />";
+        echo "<span style = color:red>No products found.</span>";
+    }
+	// To Do (DIY): End of Code
+}
+
+
+// PRODUCT TITLE/DESC + OCCASION
+else if ((isset($_GET["keywords"]) && trim($_GET['keywords']) != "") & 
+    (isset($_GET["keywords1"]) && trim($_GET['keywords1']) != "")){
+    // To Do (DIY): Retrieve list of product records with "ProductTitle" 
+    $keywords = '%' . $_GET["keywords"] . '%';
+    $result = $_GET["keywords"];
+    $keywords1 = '%' . $_GET["keywords1"] . '%';
+    $result1 = $_GET["keywords1"];
+    echo "Search results for Title/Description : <b>" . $result . "</b>| Occasion : <b>" 
+    . $result1 ."</b><br>";
+    
+    $qry = "SELECT p.ProductID, p.ProductTitle, p.ProductDesc, p.ProductImage, p.Offered, p.Price, p.OfferedPrice,
+    CASE WHEN p.Offered = 1 THEN p.OfferedPrice ELSE p.Price END AS CurrentPrice,
+    ps.SpecVal, ps.SpecID 
+    FROM product p 
+    INNER JOIN productspec ps ON p.ProductID = ps.ProductID
+    WHERE (p.ProductTitle LIKE '%$keywords%' OR p.ProductDesc LIKE '%$keywords%') 
+    AND ps.SpecID = 1
+    AND (ps.SpecVal LIKE '%$keywords1%')
+    ORDER BY ProductTitle";
+
+ 
+    $result = mysqli_query($conn, $qry);
+
+    if (mysqli_num_rows($result) > 0) {     
+        // Output the results in a table
+        echo "<table>";
+        while ($row = mysqli_fetch_assoc($result)) {
+            $productDetailsLink = "productDetails.php?pid=$row[ProductID]";
+            $img = "./Images/products/$row[ProductImage]";
+
+            echo "<img src='$img' style='width: 300px; max-width: 200px;' />";
+            echo "<div style='padding: 3%; margin-bottom: 20px;'>";
+            echo "<h3><a style='text-decoration:none' href='$productDetailsLink'>$row[ProductTitle]</a></h3>";
+            echo "<p><strong>Description:</strong> $row[ProductDesc]</p>";
+        
+            // Displaying discounted price with strike-through for the original price
+            if ($row['Offered'] == 1) {
+                echo "<p><strong>Original Price </strong> <del>$$row[Price]</del></p>";
+                echo "<p style='color:red;'><b>NOW</b> <strong>$$row[OfferedPrice]</strong></p>";
+            } else {
+                echo "<p><strong>Price</strong> $$row[Price]</p>";
+            }
+        
+            echo "<p><strong>Specification Value:</strong> $row[SpecVal]</p>";
+            echo "</div>";
+        }
+        echo "</table>";
+    } else {
+        echo "<br />";
+        echo "<span style = color:red>No products found.</span>";
+    }
+	// To Do (DIY): End of Code
+}
+
+
+// OCCASION + PRICE RANGE
+if ((isset($_GET["keywords1"]) && trim($_GET['keywords1']) != "") &
+    (isset($_GET["keywords2"]) && trim($_GET['keywords2']) != "") &
+    (isset($_GET["keywords3"]) && trim($_GET['keywords3']) != "")){
+    // To Do (DIY): Retrieve list of product records with "ProductTitle" 
+    $keywords1 = '%' . $_GET["keywords1"] . '%';
+    $result1 = $_GET["keywords1"];
+    $keywords2 = intval($_GET["keywords2"]);  // Convert to integer
+    $result2 = $_GET["keywords2"];
+    $keywords3 = intval($_GET["keywords3"]);  // Convert to integer
+    $result3 = $_GET["keywords3"];
+    echo "Search results for Occasion : <b>" . $result1 ."</b>| Maximum Price : $<b>" . $result2 .
+    "</b>| Minimum Price : $<b>" . $result3 . "</br>";
+    
+    $qry = "SELECT p.ProductID, p.ProductTitle, p.ProductDesc, p.ProductImage, p.Offered, p.Price, p.OfferedPrice,
+    CASE WHEN p.Offered = 1 THEN p.OfferedPrice ELSE p.Price END AS CurrentPrice,
+    ps.SpecVal, ps.SpecID 
+    FROM product p 
+    INNER JOIN productspec ps ON p.ProductID = ps.ProductID
+    WHERE ps.SpecID = 1
+    AND (ps.SpecVal LIKE '%$keywords1%')
+    AND ((p.Offered = 1 AND p.OfferedPrice <= $keywords2) OR (p.Offered = 0 AND p.Price <= $keywords2))
+    AND ((p.Offered = 1 AND p.OfferedPrice >= $keywords3) OR (p.Offered = 0 AND p.Price >= $keywords3))
+    ORDER BY ProductTitle";
+
+ 
+    $result = mysqli_query($conn, $qry);
+
+    if (mysqli_num_rows($result) > 0) {     
+        // Output the results in a table
+        echo "<table>";
+        while ($row = mysqli_fetch_assoc($result)) {
+            $productDetailsLink = "productDetails.php?pid=$row[ProductID]";
+            $img = "./Images/products/$row[ProductImage]";
+
+            echo "<img src='$img' style='width: 300px; max-width: 200px;' />";
+            echo "<div style='padding: 3%; margin-bottom: 20px;'>";
+            echo "<h3><a style='text-decoration:none' href='$productDetailsLink'>$row[ProductTitle]</a></h3>";
+            echo "<p><strong>Description:</strong> $row[ProductDesc]</p>";
+        
+            // Displaying discounted price with strike-through for the original price
+            if ($row['Offered'] == 1) {
+                echo "<p><strong>Original Price </strong> <del>$$row[Price]</del></p>";
+                echo "<p style='color:red;'><b>NOW</b> <strong>$$row[OfferedPrice]</strong></p>";
+            } else {
+                echo "<p><strong>Price</strong> $$row[Price]</p>";
+            }
+        
+            echo "<p><strong>Specification Value:</strong> $row[SpecVal]</p>";
+            echo "</div>";
+        }
+        echo "</table>";
+    } else {
+        echo "<br />";
+        echo "<span style = color:red>No products found.</span>";
+    }
+	// To Do (DIY): End of Code
+}
+
+
 // PRODUCT TITLE / DESC ONLY
 else if (isset($_GET["keywords"]) && trim($_GET['keywords']) != "") {
     // To Do (DIY): Retrieve list of product records with "ProductTitle" 
@@ -152,6 +326,8 @@ else if (isset($_GET["keywords"]) && trim($_GET['keywords']) != "") {
         echo "<span style = color:red>No products found.</span>";
     }
 }
+
+
 // OCCASION ONLY
 else if (isset($_GET["keywords1"]) && trim($_GET['keywords1']) != "") {
     // To Do (DIY): Retrieve list of product records with "ProductTitle" 
@@ -196,6 +372,8 @@ else if (isset($_GET["keywords1"]) && trim($_GET['keywords1']) != "") {
         echo "<span style = color:red>No products found.</span>";
     }
 }
+
+
 // RANGE ONLY
 else if ((isset($_GET["keywords2"]) && trim($_GET['keywords2']) != "") &
         (isset($_GET["keywords3"]) && trim($_GET['keywords3']) != "")) {
@@ -204,7 +382,7 @@ else if ((isset($_GET["keywords2"]) && trim($_GET['keywords2']) != "") &
     $result2 = $_GET["keywords2"];
     $keywords3 = intval($_GET["keywords3"]);  // Convert to integer
     $result3 = $_GET["keywords3"];
-    echo "Search results for Range of <b>" . $result3 . "</b>->" . $result2 . "</br>";
+    echo "Search results for Maximum Price : $<b>" . $result3 . "</b>| Minimum Price : $<b>" . $result2 . "</b></br>";
     
     $qry = "SELECT p.ProductID, p.ProductTitle, p.ProductDesc, p.ProductImage, p.Offered, p.Price, p.OfferedPrice,
     CASE WHEN p.Offered = 1 THEN p.OfferedPrice ELSE p.Price END AS CurrentPrice
